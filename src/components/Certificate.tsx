719624cc4908cf670b8bc2dcbd4bb4a1765d54cb
shayanJawer <shayanjawed4@gmail.com>
2023-04-16 15:23:09 +0000

    Changed Certificates

--- src/components/Certificate.tsx
+++ src/components/Certificate.tsx
@@ -3,12 +3,7 @@ import React from 'react'
 export default function Certificate() {
   return (
     <div>
-        <h1>Certificates</h1>
-        <div style={{height:"20px"}}></div>
-        <ul style={{listStyle:"circle"}} className='d-flex flex-column'>
-                {/* {list1?.map((e:EducationModel)=><li><h4>{e.Course} - {e.Semister} Semester</h4></li>)} */}
-                <li><a href='./assets/Certificate.pdf' target='_blank'><h4>Scrum Certificate</h4></a></li>
-        </ul>
+       Certificates
     </div>
   )
 }
