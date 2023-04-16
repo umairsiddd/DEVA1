fcf3b96e5155fa39dbe11b627876bcdb7d917c58
shayanJawer <shayanjawed4@gmail.com>
2023-04-16 16:23:47 +0000

    Work

--- src/components/Certificate.tsx
+++ src/components/Certificate.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Certificate() {
   return (
     <div>
-        <h1>Certificates</h1>
+        <h1>Certificates2</h1>
         <div style={{height:"20px"}}></div>
         <ul style={{listStyle:"circle"}} className='d-flex flex-column'>
                 {/* {list1?.map((e:EducationModel)=><li><h4>{e.Course} - {e.Semister} Semester</h4></li>)} */}
